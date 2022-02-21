e127a6bdc8551de4cc7a03703ef0c66596f1e035
Mike <eaamigk@students.eaaa.dk>
2022-02-21 13:00:25 +0100

    Second commit

--- src/app/HelloWorld.java
+++ src/app/HelloWorld.java
@@ -3,5 +3,6 @@ package app;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo, World!");
+        System.out.println("Hallo again, World!");
     }
 }
